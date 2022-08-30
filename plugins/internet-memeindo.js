e1d72c11b4255057b309a5d163fceb77a8633460
ZinxLord <90433321+ZansLord@users.noreply.github.com>
2022-08-30 12:03:47 +0700

    update internet-memeindo.js

--- plugins/internet-memeindo.js
+++ plugins/internet-memeindo.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://api.lolhuman.xyz/api/meme/memeindo?apikey=Apikeymu'
+	let url = 'https://api.lolhuman.xyz/api/meme/memeindo?apikey=ffc7159b6004dbfad13a3175'
 	conn.sendButton(m.chat, 'Mim Indo :v', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
 handler.command = /^(meme)$/i
