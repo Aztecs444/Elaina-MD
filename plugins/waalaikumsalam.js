954f0c0189f6b350eb3998ed5473d8002bdb7049
ZinxLord <90433321+ZansLord@users.noreply.github.com>
2022-08-30 12:32:41 +0700

    update waalaikumsalam.js

--- plugins/waalaikumsalam.js
+++ plugins/waalaikumsalam.js
@@ -5,9 +5,9 @@ let info = `Waalaikumsalam`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
-      mediaUrl: "https://github.com/ImYanXiao",
+      mediaUrl: "https://github.com/ZansLord",
       mediaType: 2,
-      description: "https://github.com/ImYanXiao", 
+      description: "https://github.com/ZansLord", 
       title: 'ᴇʟᴀɪɴᴀ-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
       body: wm,
       thumbnail: thumb,
