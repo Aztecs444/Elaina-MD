969162a0767c0cb73d70a5342c3fa84b8a52ae6f
ZinxLord <90433321+ZansLord@users.noreply.github.com>
2022-08-30 12:01:49 +0700

    update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,13 +4,13 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
+let str = `*https://xnxx.com/*`
 let wibu = `https://api.zacros.my.id/randomimg/waifu` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/Xiao_yan_21",
+    mediaUrl: "https://Instagram.com/black_team_indonesia",
     mediaType: "VIDEO",
-    description: "https://Instagram.com/Xiao_yan_21", 
+    description: "https://Instagram.com/black_team_indonesia", 
     title: 'Elaina-MultiDevice',
     body: wm,
     thumbnail: thumb,
